b63a16b42045c3073d99bef7bf2ca958ce54d214
David Avellaneda <g.avella@gmail.com>
2016-07-28 17:12:15 -0500

    Fix linking

--- scripts/link.js
+++ scripts/link.js
@@ -1,5 +1,6 @@
 var exec = require('child_process').exec
+var path = require('path')
 function puts(error, stdout, stderr) { console.log(stdout) }
-exec('ln -s Versions/Current/Headers src/ios/libs/Stripe.framework/Headers', puts)
-exec('ln -s Versions/Current/Modules src/ios/libs/Stripe.framework/Modules', puts)
-exec('ln -s Versions/Current/Stripe src/ios/libs/Stripe.framework/Stripe', puts)
+exec('ln -s Versions/Current/Headers ./src/ios/libs/Stripe.framework/Headers', puts)
+exec('ln -s Versions/Current/Modules ./src/ios/libs/Stripe.framework/Modules', puts)
+exec('ln -s Versions/Current/Stripe ./src/ios/libs/Stripe.framework/Stripe', puts)
