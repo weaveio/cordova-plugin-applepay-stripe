be4bb5578d9b072c8941f660e77366fc55ea1fce
David Avellaneda <g.avella@gmail.com>
2016-07-28 17:14:29 -0500

    Addimg missing symlink

--- scripts/link.js
+++ scripts/link.js
@@ -4,3 +4,4 @@ function puts(error, stdout, stderr) { console.log(stdout) }
 exec('ln -s Versions/Current/Headers ./src/ios/libs/Stripe.framework/Headers', puts)
 exec('ln -s Versions/Current/Modules ./src/ios/libs/Stripe.framework/Modules', puts)
 exec('ln -s Versions/Current/Stripe ./src/ios/libs/Stripe.framework/Stripe', puts)
+exec('ln -s A ./src/ios/libs/Stripe.framework/Versions/Current', puts)
